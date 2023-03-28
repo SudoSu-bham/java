81a868c135f06411f09f98feb3c8dbbe359a42dd
Shubham <Shubhamtiwari71488@gmail.com>
2023-03-28 11:33:44 +0530

    Passing Lambda to Thread constructor

--- Threads/labmda_thread.java
+++ Threads/labmda_thread.java
@@ -1,20 +1,24 @@
 public class labmda_thread {
     public static void main(String[] args){
-        Runnable r1 = () -> {
-                for(int i=0;i<10;i++){
-                    System.out.println("Hi");
-                    try{Thread.sleep(10);}catch(Exception e){}
-                }      
-        };
-        Runnable r2 = () -> {
-                for(int i=0;i<10;i++){
-                    System.out.println("Hello");
-                    try{Thread.sleep(10);}catch(Exception e){}
-                }
-        };
+        // Runnable r1 = () -> {
+        //         for(int i=0;i<10;i++){
+        //             System.out.println("Hi");
+        //             try{Thread.sleep(10);}catch(Exception e){}
+        //         }      
+        // };
         
-        Thread t1 = new Thread(r1);
-        Thread t2 = new Thread(r2);
+        Thread t1 = new Thread(() -> {
+            for(int i=0;i<10;i++){
+                System.out.println("Hi");
+                try{Thread.sleep(100);}catch(Exception e){}
+            }      
+    });
+        Thread t2 = new Thread(() -> {
+            for(int i=0;i<10;i++){
+                System.out.println("Hello");
+                try{Thread.sleep(100);}catch(Exception e){}
+            }
+    });
         t1.start();
         try{Thread.sleep(10);}catch(Exception e){}
         t2.start();
